7dc708acb410fe4ba88541fa8c99f5e509ebe98e
yaminmhd <mdyamin.drzee@gmail.com>
2018-05-31 18:27:30 +0800

    Initial commit

---
+++ src/DungeonGame.java
@@ -0,0 +1,4 @@
+import java.io.Serializable;
+
+public class DungeonGame implements Serializable {
+}
---
+++ src/DungeonTest.java
@@ -0,0 +1,5 @@
+public class DungeonTest {
+    public static void main(String[] args){
+
+    }
+}
