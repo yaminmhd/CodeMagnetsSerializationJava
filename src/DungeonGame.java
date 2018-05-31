e8185bd7df507650ec2cc40ebb0e6a51feee8f60
yaminmhd <mdyamin.drzee@gmail.com>
2018-05-31 18:40:55 +0800

    Added code for object serialization and deserialization

--- src/DungeonGame.java
+++ src/DungeonGame.java
@@ -1,4 +1,21 @@
 import java.io.Serializable;
 
 public class DungeonGame implements Serializable {
+    public int x = 3;
+    transient long y = 4;
+    private short z = 5;
+
+    int getX() {
+        return x;
+    }
+
+    long getY(){
+        return y;
+    }
+
+    short getZ(){
+        return z;
+    }
+
+
 }
